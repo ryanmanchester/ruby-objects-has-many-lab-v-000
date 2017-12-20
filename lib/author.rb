3219d27a3338bd966be319f4c778b32933fc0ca2
Ryan Manchester <ryanm17@gmail.com>
2017-12-20 01:16:56 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
   attr_accessor :name
+  @@post_count = 0
 
   def initialize(name)
     @name = name
@@ -13,6 +14,7 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
 
   def add_post_by_title(post_title)
