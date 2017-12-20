7945e324b698cc0d635f7f60f8bdaf56a9074755
Ryan Manchester <ryanm17@gmail.com>
2017-12-20 01:15:24 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -13,7 +13,12 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+  end
 
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    @posts << post
+    post.author = self
   end
 
 
