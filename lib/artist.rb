cd80b7c8377838570ba35e33928b57528fd6feb5
Ryan Manchester <ryanm17@gmail.com>
2017-12-20 00:50:11 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,11 +16,11 @@ class Artist
     song.artist = self
   end
 
-  def add_song_by_name(song)
+  def add_song_by_name(song_name)
     song = Song.new(name)
     @songs << song
     song.artist = self
-    song
+    song_name
     #binding.pry
   end
 
